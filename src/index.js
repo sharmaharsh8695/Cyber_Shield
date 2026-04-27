8120c2030ddf51bf8e79645249ec4e74004b0f45
Harsh Sharma <sharmaharsh8695@gmail.com>
2026-04-27 10:44:23 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 require('dotenv').config();
-
+const cors = require('cors');
 const app = express();
 const router = require('./routes')
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
